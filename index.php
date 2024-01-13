2c52239a1c6db0d18b26f2869f3facff5cef975b
Escande Guillaume <guillaume.escande@obeosoft.com>
2024-01-13 23:12:40 +0100

    init new codebase with sqlite database and login / password on json file

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 				<div class="display-4">
 					<h1 >
 						<a style="margin-right: 20px" href="/">
-						<img src="../images/logo.png" width="70" height="70"></a>TRAPTA	
+						<img src="images/logo.png" width="70" height="70"></a>TRAPTA
 					</h1>
 				</div>
 				<div class="display-4">
